b7d3bbca978313fd3fd1b4db857ce71a03b38fb7
Nikhil Komirisetti <nikhilk@live.unc.edu>
2020-04-11 16:40:35 -0400

    finished question page

--- scripts/QuestionPage.js
+++ scripts/QuestionPage.js
@@ -1,59 +1,117 @@
-let questionNumber;
-let score;
-let questions;
-let questionTimerInstance;
-
 const QuestionPageSetup = function (categoryID) {
+    let questionNumber;
+    let score;
+    let questions;
+    let questionTimerInstance;
+    let questionsCorrect;
+    let timeLeft;
+    let isAnswerClicked;
+
+    let questionContainer;
+    let scoreContainer;
+    let timeContainer;
+
     $.ajax(getQuestion(categoryID)).done(function (response) {
         questions = response.results;
         score = 0;
-        questionNumber = 0;
-        questionTimer = setInterval();
+        questionNumber = -1;
+        questionsCorrect = 0;
 
         let rootContainer = $('#rootContainer');
         rootContainer.empty();
-        rootContainer.append(buildQuestionDiv(questions[questionNumber]));
-    });
-};
 
-const buildQuestionDiv = function (question) {
-    let questionDiv = $(`<div class='questionDiv'></div>`);
-    questionDiv.append(`<div class='questionTitle'>` + question.question + `</div>`);
+        questionContainer = $(`<div class = 'questionContainer'></div>`);
+        scoreContainer = $(`<div class = 'scoreContainer'></div>`);
+        timeContainer = $(`<div class = 'timeContainer'></div>`);
 
-    let answers = [];
-
-    let correctAnswerButton = $('<button>' + question.correct_answer + '</button>');
-    correctAnswerButton.click(function () {
-        answerClicked(true);
+        rootContainer.append(scoreContainer);
+        rootContainer.append(questionContainer);
+        rootContainer.append(timeContainer);
+        setNextQuestion();
     });
 
-    answers.push(correctAnswerButton);
+    const buildQuestionDiv = function (question) {
+        isAnswerClicked = false;
+        let questionDiv = $(`<div></div>`);
+        questionDiv.append(`<div class='questionTitle'> Question ` + (questionNumber + 1) + `: ` + question.question + `</div>`);
 
-    for (const answer of question.incorrect_answers) {
-        // TODO add true false case
-        let incorrectAnswerButton = $('<button>' + answer + '</button>');
-        incorrectAnswerButton.click(function () {
-            answerClicked(false);
+        let answers = [];
+
+        let correctAnswerButton = $('<button>' + question.correct_answer + '</button>');
+        correctAnswerButton.click(function () {
+            if (isAnswerClicked === false) {
+                correctAnswerButton.addClass('correctAnswer');
+                answerClicked(true);
+            }
         });
 
-        answers.push(incorrectAnswerButton);
+        answers.push(correctAnswerButton);
+
+        for (const answer of question.incorrect_answers) {
+            // TODO add true false case
+            let incorrectAnswerButton = $('<button>' + answer + '</button>');
+            incorrectAnswerButton.click(function () {
+                if (isAnswerClicked === false) {
+                    incorrectAnswerButton.addClass('incorrectAnswer');
+                    answerClicked(false);
+                }
+            });
+
+            answers.push(incorrectAnswerButton);
+        }
+
+        shuffleArray(answers);
+        for (const answer of answers) {
+            questionDiv.append(answer);
+        }
+        return questionDiv;
     }
 
-    shuffleArray(answers);
-    for (const answer of answers) {
-        questionDiv.append(answer);
+    const setNextQuestion = function () {
+        clearInterval(questionTimerInstance);
+        questionNumber += 1;
+        // TODO make this dynamic
+        if (questionNumber == 10) {
+            ScorePageSetup(score, questionsCorrect);
+            return;
+        }
+        timeLeft = 60;
+        questionContainer.empty();
+        questionContainer.append(buildQuestionDiv(questions[questionNumber]));
+
+        scoreContainer.text(score + " Points");
+
+        questionTimerInstance = setInterval(questionTimer, 1000);
+        timeContainer.text(timeLeft);
     }
-    return questionDiv;
-}
 
-const answerClicked = function (answer) {
-    if (answer) {
-        alert('yes!');
-    } else {
-        alert('bro.');
+    const answerClicked = function (answer) {
+        isAnswerClicked = true;
+        setTimeout(function () {
+            if (answer) {
+                correctAnswer()
+            } else {
+                incorrectAnswer()
+            }
+        }, 3000);
     }
-}
 
-const questionTimer = function () {
+    const questionTimer = function () {
+        timeLeft -= 1;
+        if (timeLeft == 0) {
+            incorrectAnswer();
+        }
+        timeContainer.text(timeLeft);
+    }
 
-}
+    const correctAnswer = function () {
+        score += 50;
+        score += timeLeft;
+        questionsCorrect += 1;
+        setNextQuestion();
+    }
+
+    const incorrectAnswer = function () {
+        setNextQuestion();
+    }
+};
---
+++ scripts/ScorePage.js
@@ -0,0 +1,5 @@
+const ScorePageSetup = function (score, questionsCorrect) {
+    let rootContainer = $('#rootContainer');
+    rootContainer.empty();
+    alert("Your score is " + score + " and you got " + questionsCorrect + " questions correct");
+}
