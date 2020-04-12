eb03028120d801797b46cddeedab71aa4bed996c
Nikhil Komirisetti <nikhilk@live.unc.edu>
2020-04-12 18:41:40 -0400

    scoresAPI working

--- scripts/ScorePage.js
+++ scripts/ScorePage.js
@@ -7,17 +7,16 @@ const ScorePageSetup = function (score, questionsCorrect, categoryID) {
 
     $.ajax(postScores(name, picture, score, categoryID)).done(function (response) {
         rootContainer.empty();
-        console.log(response);
         let returnButton = $(`<button class='returnButton'>Back to Categories</button>`);
         returnButton.click(function () {
             CategoryPageSetup();
         });
 
-        const scoreResult = 'You got ' + questionsCorrect + ' question';
+        let scoreResult = 'You got ' + questionsCorrect + ' question';
         if (questionsCorrect != 1) {
             scoreResult += 's';
         }
-        scoreResult += '. You placed ' + response[0].position + nth(response[0].position) + ' for this category';
+        scoreResult += 'correct and placed ' + response[0].position + nth(response[0].position) + ' for this category';
 
         rootContainer.append(`<div class='scoreLabel'>Your score is ` + score + `</div>`);
         rootContainer.append(`<div class='scoreSubLabel'>` + scoreResult + `</div>`);
--- scripts/ScoresAPI.js
+++ scripts/ScoresAPI.js
@@ -1,4 +1,8 @@
 const postScores = function (name, spriteID, score, category) {
+    if (category === 'ALL'){
+        category = 0;
+    }
+
     return {
         "url": "https://triviawinners.herokuapp.com/score?name=" + name + "&spriteID=" + spriteID + "&score=" + score + "&category=" + category,
         "method": "POST",
